4d6a556f11e9ee2d40656ca87ba217dfc5da407f
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-09 21:26:00 +0900

    Implement Contact returns a contact's full name a string

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -41,5 +41,12 @@ describe Contact do
     expect(contact.errors[:email]).to include("has already been taken")
   end
 
-  it "returns a contact's full name as a string"
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'johndoe@example.com'
+    )
+    expect(contact.name).to eq 'John Doe'
+  end
 end
