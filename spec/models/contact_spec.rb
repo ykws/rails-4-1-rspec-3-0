11a0802df7d10aeb442b1b4bc047e2190f7d24b6
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-09 21:30:06 +0900

    Implement Contact returns a sorted array of result that match

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -49,4 +49,23 @@ describe Contact do
     )
     expect(contact.name).to eq 'John Doe'
   end
+
+  it "retunrs a sorted array of results that match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@example.com'
+    )
+    jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@example.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjohnson@example.com'
+    )
+    expect(Contact.by_letter("J")).to eq [johnson, jones]
+  end
 end
