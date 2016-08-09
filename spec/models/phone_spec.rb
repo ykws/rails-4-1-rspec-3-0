740e85e36d85a68af5ae4a309adc145bff3e6191
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-09 21:22:24 +0900

    Implement Phone allows two contacts to share a phone number

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -20,5 +20,22 @@ describe Phone do
     expect(mobile_phone.errors[:phone]).to include('has already been taken')
   end
 
-  it "allows two contacts to share a phone number"
+  it "allows two contacts to share a phone number" do
+    contact = Contact.create(
+      firstname: 'Joe',
+      lastname: 'Tester',
+      email: 'joetester@example.com'
+    )
+    contact.phones.create(
+      phone_type: 'home',
+      phone: '785-555-1234'
+    )
+    other_contact = Contact.new
+    other_phone = other_contact.phones.build(
+      phone_type: 'home',
+      phone: '785-555-1234'
+    )
+
+    expect(other_phone).to be_valid
+  end
 end
