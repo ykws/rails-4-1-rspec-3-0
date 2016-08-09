7691810b7b269600e20327c479837184bdb6b7ba
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-09 21:20:15 +0900

    Implement Phone does not allow duplicate phone number per contact

--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 describe Phone do
-  it "does not allow duplicate phone numbers per contact"
+  it "does not allow duplicate phone numbers per contact" do
+    contact = Contact.create(
+      firstname: 'Joe',
+      lastname: 'Tester',
+      email: 'joetester@example.com'
+    )
+    contact.phones.create(
+      phone_type: 'home',
+      phone: '785-555-1234'
+    )
+    mobile_phone = contact.phones.build(
+      phone_type: 'mobile',
+      phone: '785-555-1234'
+    )
+
+    mobile_phone.valid?
+    expect(mobile_phone.errors[:phone]).to include('has already been taken')
+  end
+
   it "allows two contacts to share a phone number"
 end
