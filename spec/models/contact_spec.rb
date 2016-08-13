d64568e800f9c9c70e871d57fb38fd9b4d903a5a
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-13 23:05:05 +0900

    RSepc: Contact omits results that do not match

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -68,4 +68,23 @@ describe Contact do
     )
     expect(Contact.by_letter("J")).to eq [johnson, jones]
   end
+
+  it "omits results that do not match" do
+    smith = Contact.create(
+      firstname: 'John',
+      lastname: 'Smith',
+      email: 'jsmith@exmaple.com'
+    )
+    jones = Contact.create(
+      firstname: 'Tim',
+      lastname: 'Jones',
+      email: 'tjones@exmaple.com'
+    )
+    johnson = Contact.create(
+      firstname: 'John',
+      lastname: 'Johnson',
+      email: 'jjohnson@example.com'
+    )
+    expect(Contact.by_letter("J")).not_to include smith
+  end
 end
