10dfc9ed81de86ed38d0da267adbae8743d1a484
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-14 22:07:10 +0900

    RSpec: Contacts Controller PATCH #update with invalid attributes

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -143,9 +143,20 @@ describe ContactsController do
     end
 
     context "with invalid attributes" do
-      it "does not update the contact"
+      it "does not update the contact" do
+        patch :update, id: @contact,
+          contact: attributes_for(:contact,
+            firstname: "Larry", lastname: nil)
+        @contact.reload
+        expect(@contact.firstname).not_to eq("Larry")
+        expect(@contact.lastname).to eq("Smith")
+      end
 
-      it "re-renders the :edit template"
+      it "re-renders the :edit template" do
+        patch :update, id: @contact,
+          contact: attributes_for(:invalid_contact)
+        expect(response).to render_template :edit
+      end
     end
   end
 
