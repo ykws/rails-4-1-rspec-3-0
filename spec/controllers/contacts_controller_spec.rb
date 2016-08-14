6bca41a6e22c396bcc1db040782df4c2ef219922
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2016-08-14 12:36:36 +0900

    RSpec: Contacts Controller GET #show

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -17,9 +17,17 @@ describe ContactsController do
   end
 
   describe 'GET #show' do
-    it "assigns the requested contact to @contact"
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
 
-    it "renders the :show template"
+    it "renders the :show template" do
+      contact = create(:contact)
+      get :show, id: contact
+      expect(response).to render_template :show
+    end
   end
 
   describe 'GET #new' do
